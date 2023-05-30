e621f8fcd7171f58ddf41abcad6c6f3840b92198
IrynaShadiuk <shaduk3@gmail.com>
2023-05-30 23:50:59 +0300

    creat_header

---
+++ H_w_react_tickets/hello-world/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/Header";
+
+import './app.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+     
+    </div>
+  );
+}
+
+export default App;
+

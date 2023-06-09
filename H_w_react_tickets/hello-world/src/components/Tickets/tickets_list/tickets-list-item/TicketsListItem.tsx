7204d1bebf1e06f652db3470c81bbbde198b17cd
IrynaShadiuk <shaduk3@gmail.com>
2023-06-09 12:25:53 +0300

    update

---
+++ H_w_react_tickets/hello-world/src/components/Tickets/tickets_list/tickets-list-item/TicketsListItem.tsx
@@ -0,0 +1,27 @@
+import { ITicket, TicketType } from "../../../interfaced/tickets.dao";
+import './ticketsListItem.scss'
+interface IProps {
+  title: string;
+  id: number;
+  date: Date;
+  type: TicketType;
+  price: number;
+  sold: boolean;
+}
+
+    const TicketsListItem = (props: IProps) => {
+    const { title, id, date, type, price, sold } = props;
+
+    return (
+        <tr>
+          <td className="title">{title}</td>
+          <td className="id">(ID: {id})</td>
+          <td className="date">{date.toString()}</td>
+          <td className="type">{type}</td>
+          <td className="price">${price}</td>
+          <td className="status">{sold ? "Sold" : "Not Sold"}</td>
+        </tr>
+      );
+    };
+
+export default TicketsListItem;
